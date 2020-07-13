c884e5dad70c0994b81dc5d06adb9ce28e01d694
jean-charles-gibier <jeancharlesg@free.fr>
2020-07-13 03:31:47 +0200

    First attempts in ajax #2

--- gpapp/static/js/custom.js
+++ gpapp/static/js/custom.js
@@ -12,28 +12,29 @@ $(function() {
         $('input:text').val('');
 
         $.ajax({
-            data: 'La pyramide du louvre',
+            data: 'text',
             type: 'POST',
             url: '/ask',
+//            dataType : 'json' ,
             beforeSend: function () {
                 $('.loader').show();
 //                $('#buttonsearch').hide();
 //                $('#output').fadeOut()},
+            },
             complete: function () {
                 $('.loader').hide();
 //                $('#buttonsearch').show();
 //                $('#output').fadeIn();
-//                $("ol:first").prepend("<li class='alert col-lg-6 text-lg-left text-md-left' style='background: #2c3e50; color: #19bc9c;'>" + $('#search').val()) + "</li>";}
-        })
+//                $("ol:first").prepend("<li class='alert col-lg-6 text-lg-left text-md-left' style='background: #2c3e50; color: #19bc9c;'>" + $('#search').val()) + "</li>";
+                    }
+                })
 
-//        .done(function (data) {
-//            $("ol:first").prepend(data.output);
-//        });
-    });
+        .done(function (data) {
+                $('#papyChat').append( '<div class="row"> <div class="col-lg-10"> <div class="alert text-left" style="background: #0a2929; color: #c1f0f0;">'
+                          + data.output +
+                        '</div></div></div>');
 
-    $list.on('click', '#papyChat .row', function() {
-    var $this = $(this);
-    $this.remove();
+        });
     });
 
 });
