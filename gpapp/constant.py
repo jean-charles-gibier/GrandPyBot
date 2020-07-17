007d1923d95f70c444ddd1c90a0f6306d0238aed
jean-charles-gibier <jeancharlesg@free.fr>
2020-07-17 17:36:10 +0200

    Pb sur Heroku #5

--- gpapp/constant.py
+++ gpapp/constant.py
@@ -2,10 +2,6 @@
 """
 constantes yagp
 """
-
-# KEY pour charger la map
-KEY_API_MAP_LOAD_GG = 'AIzaSyAObn2Vc7C154ZfoVKPPMLKq6pustU_fbg'
-
 # URL pour findplace (serveur)
 URL_API_FIND_PLACE_GG = 'https://maps.googleapis.com/maps/api/place/findplacefromtext/json?' \
                  'input={}&key={}&inputtype=textquery&fields=formatted_address,geometry'
