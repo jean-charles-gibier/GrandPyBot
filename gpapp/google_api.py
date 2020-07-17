251360ac6962cf7fc3a70091caf23fd43bf4801e
jean-charles-gibier <jeancharlesg@free.fr>
2020-07-17 16:38:50 +0200

    Pb sur Heroku #3

--- gpapp/google_api.py
+++ gpapp/google_api.py
@@ -21,6 +21,7 @@ class GoogleApi:
         try:
             gg_text = urllib.parse.quote(text)
             gg_key_api_find_place = os.getenv('GG_KEY_API_FIND_PLACE')
+            print("Key GG API_FIND_PLACE :'{}'".format(gg_key_api_find_place))
             gg_url = constant.URL_API_FIND_PLACE_GG.format(
                 gg_text,
                 gg_key_api_find_place)
